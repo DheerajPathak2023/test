7257d5def0460ab1af36bb69a7b79b1c2ced4e49
my-devops <dheerajpathak038@gmail.com>
2023-05-08 20:31:49 +0530

    Archtirect

---
+++ src/main/java/com/example/test/ProductService1Application.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductService1Application.class, args);
+		System.out.println("hi there");
+	}
+
+}
